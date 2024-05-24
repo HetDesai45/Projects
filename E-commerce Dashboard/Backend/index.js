306ed9c7a2f697524ef31111356578d183b3f874
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-05-24 19:31:50 +0530

    E

--- E-commerce Dashboard/Backend/index.js
+++ E-commerce Dashboard/Backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const connection = require('./connection');
+
 const app = express();
 
-connection("mongodb://127.0.0.1:27017/e-comm");
+connection("mongodb://127.0.0.1:27017/e-commerce");
 
 app.get("/", (req,res)=>{
   return res.send("Hello");
