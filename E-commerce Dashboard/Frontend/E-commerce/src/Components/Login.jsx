aced9d1b24f4e97dc6cc822cc221ecd368db54f2
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-05-26 01:19:02 +0530

    M

--- E-commerce Dashboard/Frontend/E-commerce/src/Components/Login.jsx
+++ E-commerce Dashboard/Frontend/E-commerce/src/Components/Login.jsx
@@ -1,13 +1,45 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () =>{
+const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  useEffect(()=>{
+    const auth = localStorage.getItem("user");
+    if(auth){
+      navigate('/');
+    }
+  })
+  const loginuser = async () => {
+    try {
+      let result = await axios.post(
+        "http://localhost:5000/user/login",
+        { email, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-  const [email,setEmail] = useState();
-  const [password,setPassword] = useState();
-  return(
+      localStorage.setItem("user", JSON.stringify(result));
+      result = await JSON.stringify(result);
+      result = await result.JSON();
+      if (result.name) {
+        navigate("/");
+      }else{
+        alert("Please Enter Correct details")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
     <div className="login">
       <h1>Login!</h1>
-      <form action="">
+      <form action="" onSubmit={loginuser}>
         <input
           type="email"
           name="email"
@@ -30,10 +62,10 @@ const Login = () =>{
         />
         <br />
         <br />
-        <button type="submit">Signup</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
-}
+};
 
 export default Login;
