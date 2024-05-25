f67afe6f1ef44605d1d07fd99be93de73778642e
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-05-25 21:41:42 +0530

    M

--- E-commerce Dashboard/Backend/routes/user.js
+++ E-commerce Dashboard/Backend/routes/user.js
@@ -5,12 +5,27 @@ const router = Router();
 
 router.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
-  const result = await user.create({
+  let result = await user.create({
     name,
     email,
     password,
   });
+  result = result.toObject();
+  delete result.password;
   return res.send(result);
 });
+
+router.post("/login",async(req,res)=>{
+  if(req.body.password && req.body.email){
+    const result = await user.findOne(req.body).select("-password");
+    if(result){
+      res.send(result);
+    }else{
+      res.send({Error:"No user found"})
+    }
+  }else{
+    res.send({Error:"No user found"})
+  }
+})
 
 module.exports = router;
